042ab25e5b132620b2cd1553ea40c437df65e461
YVES10BOBO <123149216+YVES10BOBO@users.noreply.github.com>
2024-11-17 10:12:33 +0200

    my third commit changes

---
+++ CONTACT_ME.html/css/style/potifolio.js
@@ -0,0 +1,27 @@
+
+//  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+// <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+
+
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+ 
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
